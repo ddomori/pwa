8354dec5513f3f83b52169650781c59898139208
dario.dominguez <dario.dominguez.orihuela@xunta.es>
2025-12-26 20:43:11 +0100

    añadido sw.js e icono

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('fetch', (event) => {
+  // Este código vacío es suficiente para "engañar" al navegador
+  // y que considere que la app tiene capacidades offline.
+  return;
+});
